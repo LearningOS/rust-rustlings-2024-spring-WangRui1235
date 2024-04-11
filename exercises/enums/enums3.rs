6efd38e97d4ff5e822804ee73c5f3335e01d2944
WangRui1235 <129173205+WangRui1235@users.noreply.github.com>
2024-04-11 04:11:09 +0000

    update

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -9,6 +9,10 @@
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    Move{x:u8, y:u8},
+    Echo(String),
+    ChangeColor(u8,u8,u8),
+    Quit
 }
 
 struct Point {
@@ -43,6 +47,12 @@ impl State {
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
         // extra parentheses: fn function((t, u, p, l, e))
+        match message {
+            
+
+
+        }
+        
     }
 }
 
